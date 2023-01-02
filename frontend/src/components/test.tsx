87ea2b87a81b0268e3f78d6c7cf73eb5dd61bdcb
Mark <mark@markg.co.uk>
2023-01-02 15:23:49 +0000

    Fix css typings

--- frontend/src/components/test.tsx
+++ frontend/src/components/test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./test.module.css";
+import * as style from "./test.module.css";
 
 export default function Test() {
   return <div className={style.test}>Hello</div>;
